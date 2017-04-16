48925b70a96419919e3c78f9c202b763256f4a20
oyh <919917215@qq.com>
2017-04-16 22:58:08 +0800

    4.16 work

--- src/components/public/footer.vue
+++ src/components/public/footer.vue
@@ -19,8 +19,27 @@
       <v-slider
         :value="curTimeNum"
         :totalVal="tolTimeNum"
-        :width="1000"></v-slider>
+        :width="950"
+        @skip="skip"
+        @move="move"></v-slider>
       <span class="time">{{tolTime}}</span>
+      <span class="value" @click="volumeOff"><i class="fa fa-fw"
+        :class="{'fa-volume-up': !isVolumeOff, 'fa-volume-off': isVolumeOff}"></i></span>
+      <v-slider
+        :value="volume"
+        :totalVal="1"
+        :width="100"
+        :style="{'margin-left': '5px'}"
+        @skip="skipVolume"></v-slider>
+      <span class="play-state"
+        @click="playStateIndex = playStateIndex === 3 ? 0 : ++playStateIndex"
+      >
+        <i class="fa fa-fw" :class="playState"></i>
+      </span>
+      <span class="list-num">
+        <span class="list-icon"><i class="fa fa-file-text-o fa-fw"></i></span>
+        {{listLength}}
+      </span>
   </div>
 </template>
 
@@ -40,15 +59,21 @@ import vSlider from '../slider.vue'
       return {
         url: '',
         id: 347230,
-        curTime: '0:00',
-        tolTime: '0:00',
+        curTime: '00:00',
+        tolTime: '00:00',
         curTimeNum: 0,
         tolTimeNum: 0,
-        timer: {}
+        volume: 1,
+        timer: {},
+        isVolumeOff: false,
+        saveVolume: 1,
+        playStateAll: ['loop', 'loopOne', 'random', 'order'],
+        playStateIndex: 0,
+        listLength: 0
       }
     },
     created() {
-      this.axios.get('http://localhost:3000/music/url?id=437250607').then(response => {
+      this.axios.get('http://localhost:3000/music/url?id=26075648').then(response => {
         this.url = response.data.data[0].url
       })
     },
@@ -56,11 +81,24 @@ import vSlider from '../slider.vue'
       this.$refs.audio.addEventListener('play', () => {
         this.tolTimeNum = this.$refs.audio && this.$refs.audio.duration
         this.tolTime = this.$refs.audio && this.formatTime(this.$refs.audio.duration)
+
+        this.timer = setInterval(() => {
+          this.curTimeNum = this.$refs.audio && this.$refs.audio.currentTime
+          this.curTime = this.$refs.audio && this.formatTime(this.$refs.audio.currentTime)
+        }, 990)
       }, false)
-      this.timer = setInterval(() => {
-        this.curTimeNum = this.$refs.audio && this.$refs.audio.currentTime
-        this.curTime = this.$refs.audio && this.formatTime(this.$refs.audio.currentTime)
-      }, 990)
+    },
+    computed: {
+      playState() {
+        let obj = {
+          loop:    'fa-rotate-right',
+          loopOne: 'fa-refresh',
+          random:  'fa-random',
+          order:   'fa-reorder'
+        }
+        let {[this.playStateAll[this.playStateIndex]]: bg} = obj
+        return bg
+      }
     },
     methods: {
       formatTime(time) {
@@ -69,6 +107,45 @@ import vSlider from '../slider.vue'
         second = second > 9 ? second : `0${second}`
         min = min > 9 ? min : `0${min}`
         return `${min}:${second}`
+      },
+      skip(skipWidth) {
+        if (skipWidth === 0) {
+          this.$refs.audio.currentTime = 0
+          this.curTimeNum = 0
+          return
+        } 
+        this.$refs.audio.currentTime = skipWidth / 950 * this.tolTimeNum
+        this.curTimeNum = this.$refs.audio.currentTime
+      },
+      skipVolume(skipWidth) {
+        if (skipWidth === 0) {
+          this.$refs.audio.volume = 0
+          this.volume = 0
+          this.saveVolume = 0
+          return 
+        }
+        this.$refs.audio.volume = skipWidth / 100 * 1
+        this.volume = this.$refs.audio.volume
+        this.saveVolume = this.volume
+      },
+      volumeOff() {
+        this.isVolumeOff = !this.isVolumeOff
+        if (this.isVolumeOff) {
+          this.$refs.audio.volume = 0
+          this.volume = 0
+        } else {
+          this.$refs.audio.volume = this.saveVolume
+          this.volume = this.$refs.audio.volume
+        }
+      },
+      move(value) {
+        if (value === 0) {
+          this.$refs.audio.currentTime = 0
+          this.curTimeNum = 0
+          return
+        } 
+        this.$refs.audio.currentTime = value / 950 * this.tolTimeNum
+        this.curTimeNum = this.$refs.audio.currentTime
       }
     }
   }
--- src/components/slider.vue
+++ src/components/slider.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="progress-container" @mousedown="skip" ref="progress">
+  <div class="progress-container"
+    @mousedown="skip"
+    ref="progress"
+    :style="{width: `${width}px`}">
     <div class="progress" :style="{width: `${proWidth}px`}"></div>
-    <div class="dot" :style="{left: `${proWidth - 7.5}px`}">
+    <div class="dot" 
+      :style="{left: `${proWidth - 7.5}px`}" 
+      @mousedown="moveInit"
+      ref="dot"
+    >
       <span class="circle"></span>
     </div>
   </div>
@@ -31,26 +38,62 @@ export default {
   },
   data() {
     return {
-
+      curValue: this.value,
+      flag: false
     }
   },
   computed: {
-    curTime() {
-      return this.value
-    },
     proWidth() {
-      return this.curTime / this.totalVal * this.width
+      return parseFloat((this.curValue / this.totalVal * this.width).toFixed(3))
     }
   },
   methods: {
-    move() {
+    moveInit(e) {
+      this.flag = false
+      document.addEventListener('mousemove', this.moveHandler, false)
+      document.addEventListener('mouseup', this.upHandler, false)
+    },
+    /*move(e) {
+      let moveWidth = e.clientX - this.$refs.progress.offsetLeft
+      if (moveWidth < 0) return this.$emit('move', 0)
+      if (moveWidth > this.width) return this.$emit('move', this.width)
+      return this.$emit('move', moveWidth)
+    },*/
+    moveHandler(e) {
+      if (this.flag) {
+        document.removeEventListener('mousemove', this.moveHandler, false) 
+        console.log(22222)
+        return
+      }
+      let moveWidth = this.$refs.progress && e.clientX - this.$refs.progress.offsetLeft
+        console.log(moveWidth)
 
+      if (moveWidth < 0) {
+        this.curValue = 0
+      } else if (moveWidth > this.width) {
+        this.curValue = this.totalVal
+      } else {
+        this.curValue = this.moveWidth
+      }
+    },
+    upHandler(e) {
+      console.log(1)
+      this.flag = true
+      document.removeEventListener('mousemove', this.moveHandler, false)
+      document.removeEventListener('mouseup', this.upHandler, false)
     },
     skip(e) {
       let skipWidth = e.clientX - this.$refs.progress.offsetLeft
       if (skipWidth < -3) return
-      if (skipWidth > -3 && skipWidth < 0) return 0
-      return skipWidth
+      if (skipWidth > -3 && skipWidth < 0) return this.$emit('skip', 0)
+      return this.$emit('skip', skipWidth)
+    }
+  },
+  watch: {
+    value: {
+      handler(newVal) {
+        this.curValue = newVal
+      }
     }
   }
 }
